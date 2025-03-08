2c67a9de4405db341477d98b9faf9728a50c7015
ashutosh <ashutosh.choudhary_cs21@gla.ac.in>
2025-03-08 12:29:54 +0530

    Added Service with getmethods

--- src/main/java/com/AddressBook/Address/interfaces/IAddressService.java
+++ src/main/java/com/AddressBook/Address/interfaces/IAddressService.java
@@ -1,12 +1,11 @@
 package com.AddressBook.Address.interfaces;
 
-import com.AddressBook.Address.model.Address;
-
+import com.AddressBook.Address.dto.AddressDTO;
 import java.util.List;
 
 public interface IAddressService {
-    Address save(Address address);
-    List<Address> getAll();
-    Address getById(Long id);
+    AddressDTO save(AddressDTO addressDTO);
+    List<AddressDTO> getAll();
+    AddressDTO getById(Long id);
     void delete(Long id);
 }
--- src/main/java/com/AddressBook/Address/service/AddressService.java
+++ src/main/java/com/AddressBook/Address/service/AddressService.java
@@ -1,5 +1,6 @@
 package com.AddressBook.Address.service;
 
+import com.AddressBook.Address.dto.AddressDTO;
 import com.AddressBook.Address.interfaces.IAddressService;
 import com.AddressBook.Address.model.Address;
 import com.AddressBook.Address.repository.AddressRepository;
@@ -7,8 +8,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AddressService implements IAddressService {
 
+    private final AddressRepository addressRepository;
+
+    @Autowired
+    public AddressService(AddressRepository addressRepository) {
+        this.addressRepository = addressRepository;
+    }
+
+
+    @Override
+    public List<AddressDTO> getAll() {
+        return addressRepository.findAll().stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public AddressDTO getById(Long id) {
+        return addressRepository.findById(id)
+                .map(this::convertToDTO)
+                .orElse(null);
+    }
+
+
+    private AddressDTO convertToDTO(Address address) {
+        return new AddressDTO(address.getId(), address.getName(), address.getPhone(), address.getEmail(), address.getCity());
+    }
+
+    private Address convertToEntity(AddressDTO dto) {
+        return new Address(dto.getId(), dto.getName(), dto.getPhone(), dto.getEmail(), dto.getCity());
+    }
 }
